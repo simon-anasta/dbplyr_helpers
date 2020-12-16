a0713b2d6def5114b256414b3a7028cc267cbbc0
simon-anasta <simon.anastasiadis@gmail.com>
2020-12-16 20:24:39 +1300

    updates to support functions and tests

--- automated_tests.R
+++ automated_tests.R
@@ -1,33 +1,36 @@
 ###############################################################################
-#' Description: Automated test suite for dbplyr helpers and utility functions
+#' Description: Automated test suite for helper functions
+#' Outputs automated test results
 #'
-#' Input: 
-#'
-#' Output: Automated test results
-#' 
 #' Author: Simon Anastasiadis
-#' 
-#' Dependencies:
-#' 
+#'
 #' Notes:
 #'  - Working directory must contain this file, folder of test scripts, and the code files to test
 #'  - Sourcing the file runs all scripts (2 minutes), individual tests scripts can be run in order line by line
 #'  - Uses code folding by headers (Alt + O to collapse all)
-#'    
-#' Issues:
-#'  - 2020-01-06 produces rlang warnings on first run each session due to function depreciation.
-#'    We do not use rlang package directly, so this warning is caused by a package that depends on rlang.
-#' 
-#' History (reverse order):
-#' 2020-01-06 SA v1
-#' 2019-12-12 SA v0
-#'#############################################################################
+#'
+#' #############################################################################
+
+## setup ------------------------------------------------------------------------------------------
+
+# working directory
+PATH_TO_SCRIPTS <- "~/Path/To/Folder/Containing/R/Files"
+
+# schema for storing user created table
+our_schema <- "[]"
+# database for storing user created tables
+our_db <- "[]"
+# database for creating use created views
+our_usercode <- "[]"
+
+setwd(PATH_TO_SCRIPTS)
 
 ## test everything --------------------------------------------------------------------------------
 #
 # For quick runs of all tests when checking that nothing has broken
 
 source("utility_functions.R")
+source("table_consistency_checks.R")
 source("dbplyr_helper_functions.R")
 testthat::test_dir("./tests")
 
@@ -35,14 +38,20 @@ testthat::test_dir("./tests")
 #
 # runs each test file in the recommended order
 # (later tests depend on functionality from earlier tests)
-# 
+#
 # Intended for stepping through code, does not run on source
 
-if(FALSE){
+if (FALSE) {
   # test utility_functions.R
   source("utility_functions.R")
   testthat::test_file("./tests/test_UF_independent_functions.R")
-  
+
+  # test table consistency checkers
+  source("table_consistency_checks.R")
+  testthat::test_file("./tests/test_TCC_base_and_size.R")
+  testthat::test_file("./tests/test_TCC_uniques_and_joins.R")
+  testthat::test_file("./tests/test_TCC_missings_and_overlaps.R")
+
   # test dbplyr helpers
   #
   # excludes tests of the functions: create_clustered_index, write_for_reuse
@@ -53,8 +62,3 @@ if(FALSE){
   testthat::test_file("./tests/test_DHF_other_writing.R")
   testthat::test_file("./tests/test_DHF_manipulations.R")
 }
-
-## tidy up ----------------------------------------------------------------------------------------
-
-# remove folder for temporary SQL scripts
-unlink("./tests/SQL tmp scripts", recursive = TRUE)
